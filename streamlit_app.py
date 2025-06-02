dc37bba2a575797068f9b9e6b633f4f4d3699145
malo1557 <168790964+malo1557@users.noreply.github.com>
2025-06-02 02:32:51 +0000

    tesr1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,22 +1,20 @@
-%%writefile app.py
-
 import streamlit as st
 from streamlit_folium import folium_static
 import folium
 from folium.plugins import MarkerCluster
 import pandas as pd
 
-
 st.title("진주시 CCTV 현황")
 
-df = pd.read_csv("/content/경상남도 진주시_CCTV위치정보_20250501.csv", encoding='euc-kr')
+# 로컬 경로로 수정
+df = pd.read_csv("경상남도 진주시_CCTV위치정보_20250501.csv", encoding='euc-kr')
 
 st.dataframe(df, height=200)
 
+# 위도, 경도 컬럼 이름 확인 후 수정 필요 시 여기서
 df[["lat","lon"]] = df[["위도","경도"]]
 
 m = folium.Map(location=[35.1799817, 128.1076213], zoom_start=13)
-
 marker_cluster = MarkerCluster().add_to(m)
 
 for idx, row in df.iterrows():
